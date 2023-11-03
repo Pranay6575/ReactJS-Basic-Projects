78f8f9739f6da0f69085eca4abca5cfe5d60f1d2
Pranay <pranaypatil6575@gmail.com>
2023-11-03 16:44:12 +0530

    project-5 50% done

--- project-5/src/components/Modal.jsx
+++ project-5/src/components/Modal.jsx
@@ -1,15 +1,17 @@
 import { AiOutlineClose } from "react-icons/ai";
+import { createPortal } from "react-dom";
 const Modal = ({ open, close, children }) => {
-  return (
+  return createPortal(
     <>
       {open && (
         <>
-          <div className="relative m-auto z-50 min-h-[200px] max-w-[80%] bg-white">
+          <div className="relative m-auto z-40 min-h-[200px] max-w-[80%] bg-white">
             <div className="flex justify-end p-3 cursor-pointer">
               <AiOutlineClose onClick={close} className="text-2xl" />
             </div>
+            {children}
           </div>
-          <div onClick={close} className="absolute top-0 z-40 h-screen w-screen backdrop-blur " />
+          <div onClick={close} className="absolute top-0 z-30 h-screen w-screen backdrop-blur " />
         </>
       )}
     </>
